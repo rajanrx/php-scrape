21e7786415762bdd8f7291e2ffb2ba33c48673c3
rajan <rajan.inception.software@gmail.com>
2015-11-28 11:02:14 +1100

    set default timezone to UTC

--- Examples/git-top-repos.php
+++ Examples/git-top-repos.php
@@ -4,6 +4,7 @@ use Scraper\Scrape\Crawler\Types\GeneralCrawler;
 use Scraper\Scrape\Extractor\Types\MultipleRowExtractor;
 
 require_once(__DIR__ . '/../vendor/autoload.php');
+date_default_timezone_set('UTC');
 
 $crawler   = new GeneralCrawler('https://github.com/trending');
 $path      = __DIR__ . "/Data/git-repo.json";
