b3342460b275d76e6218c5220fafc8f4deeb245b
Rajan <rajanrauniyar@gmail.com>
2017-06-18 15:34:47 +1000

    minor

--- .phan/config.php
+++ .phan/config.php
@@ -2,7 +2,4 @@
 
 $config = require(__DIR__ . '/config.critical.php');
 
-// Remove the severity (which would be critical only) back to the Phan default.
-unset($config['minimum_severity']);
-
 return $config;
